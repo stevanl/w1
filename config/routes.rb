6fb2bb59c8151a26e5ec4381361dc98495f8ce8e
Stevan Litobac <stevan@litobac.com>
2014-01-09 00:58:58 +0000

    [BUG] Silly route ommission

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ W1Dating::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-  get 'home#about', as: :about
+  get 'about' => 'home#about', as: :about
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
